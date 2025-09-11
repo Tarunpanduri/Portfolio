e0992ec01976f7ac2e215824fb2379f926337913
Tarunpanduri <Tarunpanduri88@gmail.com>
2025-09-11 11:48:15 +0530

    l u o 10/10/25 12pm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { AboutComponent } from './components/about/about.component';
-import { ResumeComponent } from './components/resume/resume.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { ContactComponent } from './components/contact/contact.component';
+
 
 import { FormsModule } from '@angular/forms';
 import { V2homeComponent } from './components/v2home/v2home.component';
@@ -20,12 +15,6 @@ import { AnimateOnScrollDirective } from './directives/AnimateOnScroll/animate-o
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent,
-    NavbarComponent,
-    AboutComponent,
-    ResumeComponent,
-    PortfolioComponent,
-    ContactComponent,
     V2homeComponent,
     V2aboutComponent,
     AnimateOnScrollDirective 
